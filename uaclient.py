526273d06a96232ed1538ff0f9432066ef1db123
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-12-05 12:02:28 +0100

    XML terminado

--- uaclient.py
+++ uaclient.py
@@ -4,13 +4,33 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-from uaserver.py import XMLhandler
+from uaserver import XMLhandler
 
 import sys
+import uaserver
 
 if __name__ == "__main__":
 
+    xml = sys.argv[1]
+    Metodo = sys.argv[2]
+    Opcion = sys.argv[3]
+    
+    
+    if len(sys.argv) > 4:
+        print("Usage: python uaclient.py config method option") 
+        sys.exit()
+    
+    #######XML#######
+    ###################################################################################            
+    parser = make_parser()
+    Xml_Handler = uaserver.XMLhandler()
+    parser.setContentHandler(Xml_Handler)
+    
     try:
-        xml = sys.argv[1]
-        Metodo = sys.argv[2]
-        Opcion = sys.argv[3] 
+        parser.parse(open(xml))
+        mytags = Xml_Handler.get_tags()
+    except FileNotFoundError: 
+       print("Usage: python uaclient.py config method option")
+       sys.exit()
+    print(mytags)
+    ###################################################################################
