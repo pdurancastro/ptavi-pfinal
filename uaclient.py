ff6edf00b56e9f4123f9ed28f68219f8a5eb9fec
Patricia Duran Castro <pcastro@gamma05.aulas.gsyc.urjc.es>
2016-12-05 11:02:56 +0100

    Implementando XML

--- uaclient.py
+++ uaclient.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+from uaserver.py import XMLhandler
+
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        xml = sys.argv[1]
+        Metodo = sys.argv[2]
+        Opcion = sys.argv[3] 
