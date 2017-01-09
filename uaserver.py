f02c326ba6b4ee1550858960dfa3f02d09cf945b
pdurancastro <patryharuno@gmail.com>
2017-01-09 19:53:49 +0100

    implementandoservidor

--- uaserver.py
+++ uaserver.py
@@ -5,6 +5,9 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 import sys
+import socket
+import socketserver
+
 
 #Defino la clase que tratara mi xml
 class XMLhandler(ContentHandler):
@@ -38,12 +41,23 @@ class XMLhandler(ContentHandler):
                 
     def get_tags(self):
         return self.diccionario_xml
+        
+class EchoHandler(socketserver.DatagramRequestHandler):
+    def handle(self):
+        #while 1:
+            #print("HOLA")
+        client = self.rfile.read().decode('utf-8').split()
+        print("CLIENTE ----->")
+        print(client)
+        
+
+        
 
 
 if __name__ == "__main__":
-    parser = make_parser()
-    cHandler = XMLhandler()
+    
     xml = sys.argv[1] 
+    
     
     
     if len(sys.argv) != 2:
@@ -51,7 +65,9 @@ if __name__ == "__main__":
         sys.exit()
     
     #######XML#######
-    ###################################################################################    
+    ###################################################################################
+    parser = make_parser()
+    cHandler = XMLhandler()    
     parser.setContentHandler(cHandler)   
     try:
         #Me abrira cualquier xml en funcion del argumento que le pase
@@ -66,7 +82,31 @@ if __name__ == "__main__":
     ###################################################################################
     
     
+    
+    
+    #########SERVIDOR_PROXY############################################################
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    diccionario = mytags
+    
+    Proxy_IP = diccionario['regproxy_ip']
+    Proxy_Puerto = int(diccionario['regproxy_puerto'])
+    Log = diccionario['log_path']
+    
+    my_socket.connect((Proxy_IP, Proxy_Puerto))
+    
+    IP_Server = diccionario['uaserver_ip']
+    print(IP_Server)
+    Port_server = int(diccionario['uaserver_puerto'])
+    print(Port_server)
+    
+    
+    
+    
+    serv = socketserver.UDPServer((IP_Server, Port_server), EchoHandler)
     print("Listening...")
+    serv.serve_forever()
+    ##################################################################################
     
     
     
